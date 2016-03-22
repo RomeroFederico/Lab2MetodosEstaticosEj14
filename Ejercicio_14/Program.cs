3be238ccc3bbc472ef1e168e0551a7efc6a487cc
RomeroFederico <romerof14@gmail.com>
2016-03-22 18:45:20 -0300

    Main y clase CalculoDeArea creadas.

---
+++ Ejercicio_14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_14
+{
+    /*
+    14.  Realizar una clase llamada 'CalculoDeArea' que posea 3 métodos de 
+    clase (estáticos), double CalcularCuadrado(double), double 
+    CalcularTriangulo(double, double) y double 
+    CalcularCirculo(double), que realicen el cálculo del área que 
+    corresponda.  
+    El ingreso de los datos como la visualización se deberán realizar 
+    desde el método Main().
+    */
+
+    class Ejercicio_14
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro. 14";
+        }
+    }
+}
