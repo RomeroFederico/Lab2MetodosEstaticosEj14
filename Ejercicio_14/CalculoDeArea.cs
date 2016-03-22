bc05af8198453c122e29fc6a9d46d772195ef24c
RomeroFederico <romerof14@gmail.com>
2016-03-22 19:05:38 -0300

    Agregados metodos a la clase CalculoDeArea.
    - CalcularCuadrado().
    - CalcularTriangulo().
    - CalcularCirculo().

--- Ejercicio_14/CalculoDeArea.cs
+++ Ejercicio_14/CalculoDeArea.cs
@@ -7,5 +7,39 @@ namespace Ejercicio_14
 {
     class CalculoDeArea
     {
+        /// <summary>
+        /// Calcula el area del cuadrado.
+        /// </summary>
+        /// <param name="lado">
+        /// Lado del cuadrado.
+        /// </param>
+        public static double CalcularCuadrado(double lado)
+        {
+            return Math.Pow(lado, 2);
+        }
+
+        /// <summary>
+        /// Calcula el area del triangulo.
+        /// </summary>
+        /// <param name="b">
+        /// Base del triangulo.
+        /// </param>
+        /// Altura del triangulo.
+        /// <param name="h"></param>
+        public static double CalcularTriangulo(double b, double h)
+        {
+            return b * h / 2;
+        }
+
+        /// <summary>
+        /// Calcula el area del circulo.
+        /// </summary>
+        /// <param name="radio">
+        /// Radio del circulo.
+        /// </param>
+        public static double CalcularCirculo(double radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
     }
 }
